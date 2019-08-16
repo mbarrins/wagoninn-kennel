2183f6d5c5d9608898a930a8d065dbcbf91013f2
mbarrins <maija.barrins@gmail.com>
2019-08-16 17:54:03 +0100

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar';
+
+const Footer = () => {
+  return (
+    <Navbar fixed="bottom" bg="light" expand="lg"></Navbar>
+  )
+}
+
+export default Footer;
