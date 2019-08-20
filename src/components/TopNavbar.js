3fb240ff421df44d10e04f125ac03e9914f83866
mbarrins <maija.barrins@gmail.com>
2019-08-20 21:55:17 +0100

    corrected Reports text color

--- src/components/TopNavbar.js
+++ src/components/TopNavbar.js
@@ -8,7 +8,7 @@ import Button from 'react-bootstrap/Button';
 
 const TopNavbar = ({ isAuthenticated, logoutUser }) => {
   return (
-    <Navbar expand="lg">
+    <Navbar expand="lg" >
       <Navbar.Brand href="/main" >
         <img
           alt=""
@@ -21,7 +21,7 @@ const TopNavbar = ({ isAuthenticated, logoutUser }) => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       {isAuthenticated ?
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
+          <Nav className="mr-auto" >
             <Nav.Link className='text-dark' href="/main">Home</Nav.Link>
             <Nav.Link className='text-dark' href="/owners/new">New Boarder</Nav.Link>
             <Nav.Link className='text-dark' href="/search">Search</Nav.Link>
@@ -29,10 +29,6 @@ const TopNavbar = ({ isAuthenticated, logoutUser }) => {
               <NavDropdown.Item className='text-dark' href="/reports/occupancy">Occupancy</NavDropdown.Item>
               <NavDropdown.Item className='text-dark' href="/reports/monthly">Monthly Income</NavDropdown.Item>
               <NavDropdown.Item className='text-dark' href="/reports/compare_years">Last 3 Years Income</NavDropdown.Item>
-              {/* <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item> */}
             </NavDropdown>
           </Nav>
           <Nav className="ml-auto">
