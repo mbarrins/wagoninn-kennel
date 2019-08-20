ddd96d32f4ad232e7e1a4b6c4480bd04e20274bc
mbarrins <maija.barrins@gmail.com>
2019-08-20 16:28:19 +0100

    removed pens & pets from owner display bookings

--- src/components/OwnerBookings.js
+++ src/components/OwnerBookings.js
@@ -35,26 +35,6 @@ const OwnerBookings = ({ bookings, pets, lookups, edit }) => {
               </Col>
             </Row>
           </Col>
-          <Col>
-            <Row>
-              {booking.booking_pens.map(pen => (
-                <Col className='text-center' key={`booking_pen${pen.id}`} >
-                  <Row>
-                    <Col xs={5} className='text-left'>
-                      <h6>{lookups.penTypes.length > 0 && lookups.penTypes.find(type => type.id === parseInt(pen.pen_type_id, 10)).name}</h6>
-                    </Col>
-                    <Col xs={7} className='text-left'>
-                      {pen.booking_pen_pets.map(pen_pet => (
-                        <Row key={`booking_pen_pen_pet${pen_pet.pet_id}`}>
-                          <Col className='text-rpen_ight'><h6>{pets.find(pet => pet.id === pen_pet.pet_id).name}</h6></Col>
-                        </Row>
-                      ))}
-                    </Col>
-                  </Row>
-                </Col>
-              ))}
-            </Row>
-          </Col>
         </Row>
       ))}
     </Card.Body>
